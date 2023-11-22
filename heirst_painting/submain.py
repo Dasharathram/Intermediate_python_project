cd1ee0fe4c531c53a14560788163b4cd8d4ad694
dasharath_kota <kotadasarath@gmail.com>
2023-11-22 12:21:11 -0500

    add etch a sketch methods in main

--- heirst_painting/submain.py
+++ heirst_painting/submain.py
@@ -1,7 +1,8 @@
-# import colorgram
+import colorgram
 import random
 import turtle
 from turtle import Turtle as t, Screen as s
+
 turtle.colormode(255)
 colors_rgb_list = [(240, 242, 245), (223, 236, 228), (236, 230, 216), (140, 176, 207), (25, 32, 48), (26, 107, 159),
                    (237, 225, 235), (209, 161, 111), (144, 29, 63), (230, 212, 93), (4, 163, 197), (218, 60, 84),
@@ -12,39 +13,37 @@ print(scoop)
 scoop.shape("arrow")
 
 
-#
-#
-# def color_generator():
-#     colors = colorgram.extract("48_001.jpg", 18)
-#     colors_rgb_list = []
-#     for obj_color in colors:
-#         color_rgb = obj_color.rgb
-#         final = (color_rgb.r, color_rgb.g, color_rgb.b)
-#         return colors_rgb_list.append(final)
+def color_generator():
+    colors = colorgram.extract("48_001.jpg", 18)
+    colors_rgb_list = []
+    for obj_color in colors:
+        color_rgb = obj_color.rgb
+        final = (color_rgb.r, color_rgb.g, color_rgb.b)
+        return colors_rgb_list.append(final)
 
 
 def color_picker():
     return random.choice(colors_rgb_list)
 
 
-# def heirst_painting():
-#     i = 0
-#     while i < 100:
-#         scoop.speed(2)
-#         scoop.pensize(20)
-#         for j in range(10):
-#             scoop.color(color_picker())
-#             scoop.forward(1)
-#             scoop.up()
-#             scoop.forward(50)
-#             scoop.down()
-#             j += 1
-#         scoop.right(90)
-#         scoop.up()
-#         scoop.forward(50)
-#         scoop.right(90)
-#         scoop.down()
-#         i += 1
+def heirst_painting():
+    i = 0
+    while i < 100:
+        scoop.speed(2)
+        scoop.pensize(20)
+        for j in range(10):
+            scoop.color(color_picker())
+            scoop.forward(1)
+            scoop.up()
+            scoop.forward(50)
+            scoop.down()
+            j += 1
+        scoop.right(90)
+        scoop.up()
+        scoop.forward(50)
+        scoop.right(90)
+        scoop.down()
+        i += 1
 
 
 def heirst_painting():
@@ -68,8 +67,6 @@ def heirst_painting():
             scoop.left(90)
             scoop.forward(500)
             scoop.setheading(0)
-
-
 
 
 heirst_painting()
--- main.py
+++ main.py
@@ -5,93 +5,129 @@ from heirst_painting import submain
 
 turtle.colormode(255)
 scoop = t()
+my_screen = s()
+
 print(scoop)
 scoop.shape("arrow")
+scoop.speed(5)
 
-
-# def random_color():
-#     r = random.randint(0, 255)
-#     g = random.randint(0, 255)
-#     b = random.randint(0, 255)
-#     return r, g, b
-
-
-# list_color = ["chartreuse", "peru", "lime green", "steel blue", "firebrick", "magenta", "sandy brown", "sky blue",
-#              "black", "olive drab", "dark turquoise"]
-# for i in range(4):
-#     scoop.forward(100)
-#     scoop.left(80)
-# for _ in range(15):
-#     scoop.forward(10)
-#     scoop.penup()
-#     scoop.forward(10)
-#     scoop.pendown()
 
 def color_picker():
     return random.choice(submain.colors_rgb_list)
 
 
-#
-# def polygon_draw(move=100, sides=3):
-#     while sides < 10:
-#         color = color_picker()
-#         for side in range(sides):
-#             scoop.color(color)
-#             scoop.forward(move)
-#             angle = 360 / sides
-#             scoop.right(angle)
-#         sides += 1
-# polygon_draw()
+color_picker()
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return r, g, b
+
+
+list_color = ["chartreuse", "peru", "lime green", "steel blue", "firebrick", "magenta", "sandy brown", "sky blue",
+              "black", "olive drab", "dark turquoise"]
+for i in range(4):
+    scoop.forward(100)
+    scoop.left(80)
+for _ in range(15):
+    scoop.forward(10)
+    scoop.penup()
+    scoop.forward(10)
+    scoop.pendown()
+
+
+def polygon_draw(move=100, sides=3):
+    while sides < 10:
+        color = color_picker()
+        for side in range(sides):
+            scoop.color(color)
+            scoop.forward(move)
+            angle = 360 / sides
+            scoop.right(angle)
+        sides += 1
+
+
+polygon_draw()
+
 
 def direction_picker():
     d_list = [0, 90, 180, 270]
     return random.choice(d_list)
 
 
-#
-# def random_walk(move=30):
-#     i = 0
-#     while i < 200:
-#         scoop.color(random_color())
-#         scoop.pensize(15)
-#         scoop.speed(10)
-#         scoop.setheading(direction_picker())
-#         scoop.forward(move)
-#         i += 1
-#
-#
-# random_walk()
-# def randomnum():
-#     return random.randint(0, 360)
-#
-#
-# def draw_circles():
-#     i = 0
-#     while i < 100:
-#         scoop.color(random_color())
-#         scoop.speed("fastest")
-#         scoop.seth(randomnum())
-#         scoop.circle(100)
-#
-#         i += 1
-#
-#
-# draw_circles()
-
-#
-# def draw_spirograph(size_of_gap):
-#     for _ in range(int(360 / size_of_gap)):
-#         scoop.color(random_color())
-#         scoop.speed("fastest")
-#         scoop.circle(100)
-#         scoop.seth(scoop.heading() + size_of_gap)
+def random_walk(move=30):
+    i = 0
+    while i < 200:
+        scoop.color(random_color())
+        scoop.pensize(15)
+        scoop.speed(10)
+        scoop.setheading(direction_picker())
+        scoop.forward(move)
+        i += 1
 
 
-# draw_spirograph(2)
+random_walk()
 
 
+def randomnum():
+    return random.randint(0, 360)
 
 
-my_screen = s()
-print(my_screen.canvheight)
+def draw_circles():
+    i = 0
+    while i < 100:
+        scoop.color(random_color())
+        scoop.speed("fastest")
+        scoop.seth(randomnum())
+        scoop.circle(100)
+
+        i += 1
+
+
+draw_circles()
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        scoop.color(random_color())
+        scoop.speed("fastest")
+        scoop.circle(100)
+        scoop.seth(scoop.heading() + size_of_gap)
+
+
+draw_spirograph(2)
+
+
+def move_forwards():
+    scoop.forward(20)
+
+
+def move_right():
+    scoop.right(10)
+
+
+def move_left():
+    scoop.left(10)
+
+
+def draw_circle():
+    scoop.circle(100)
+
+
+def clear():
+    scoop.clear()
+    scoop.penup()
+    scoop.home()
+    scoop.down()
+
+
+my_screen.listen()
+my_screen.onkey(move_forwards, "space")
+my_screen.onkey(move_right, "Right")
+my_screen.onkey(move_right, "Left")
+my_screen.onkey(draw_circle, "a")
+my_screen.onkey(clear, "c")
+
 my_screen.exitonclick()
